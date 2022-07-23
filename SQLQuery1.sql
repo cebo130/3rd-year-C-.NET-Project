a9e2262f7e3cd32f577a1ebd6d7ddefd633dd9e1
cebo130 <58163646+cebo130@users.noreply.github.com>
2022-07-23 23:25:43 +0200

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,3 @@
+﻿select * from customer;
+insert into customer(name, surname, phone_number, email, password) values('Cebo', 'ryza', '0617796119', 'ceboryza@gmail.com', 'abcde');
+select * from customer;
